354057c44246ced306623874ab8c46c47ca3357a
Degumezily <phamdongdon@gmail.com>
2025-03-14 22:55:40 +0700

    script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const audio01 = document.getElementById('a01');
 const nvpic = document.getElementById('nvp')
 
-nvpic.setAttribute('onclick', 'alert("Nhật Vũ bị GAY!!!")')
 audio01.volume = 0.2
-
+nvpic.setAttribute('onclick', 'alert("Nhật Vũ bị GAY!!!")')
